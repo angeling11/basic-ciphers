76aa3f5201bfc3b7184936b4a20413d1c4c180c0
angeling11 <33943620+angeling11@users.noreply.github.com>
2023-07-23 19:42:02 -0600

    Update tel2txt.py imprive readability

--- tel2txt.py
+++ tel2txt.py
@@ -3,37 +3,37 @@
 import sys
 
 keyboard = {
-        "0": [" "],
-        "1": [""],
-        "2": ["A", "B", "C"],
-        "3": ["D", "E", "F"],
-        "4": ["G", "H", "I"],
-        "5": ["J", "K", "L"],
-        "6": ["M", "N", "O"],
-        "7": ["P", "Q", "R", "S"],
-        "8": ["T", "U", "V"],
-        "9": ["W", "X", "Y", "Z"]
+    "0": [" "],
+    "1": [""],
+    "2": ["A", "B", "C"],
+    "3": ["D", "E", "F"],
+    "4": ["G", "H", "I"],
+    "5": ["J", "K", "L"],
+    "6": ["M", "N", "O"],
+    "7": ["P", "Q", "R", "S"],
+    "8": ["T", "U", "V"],
+    "9": ["W", "X", "Y", "Z"]
 }
 
 if len(sys.argv) > 1:
-        inp = sys.argv[1]
+    inp = sys.argv[1]
 else:
-        inp = sys.stdin.read()
+    inp = sys.stdin.read()
 
 out = ""
 n = 0
+inp = inp + " "
 
 while n < len(inp):
-        if inp[n].isnumeric():
-                i = 0
-                aux = inp[n]
-                while inp[n] == aux:
-                        i += 1
-                        n += 1
-                out += keyboard[aux][i - 1]
-        else:
-                out += inp[n]
-                n += 1
+    if inp[n].isnumeric():
+        i = 0
+        aux = inp[n]
+        while inp[n] == aux:
+            i += 1
+            n += 1
+        out += keyboard[aux][i - 1]
+    elif inp[n] != " ":
+        out += inp[n]
+    n += 1
 
-print("Text Decoded:")
-print(out)
+print("Text Decoded:", out)
